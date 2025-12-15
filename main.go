48840771ea8dff36cd4d8b473777e518d29ffaee
Emirhan Sevmez <emirhansevmezz@gmail.com>
2025-12-15 19:38:27 +0300

    feat: implement web scraping functionality with URL input and save output files

--- main.go
+++ main.go
@@ -1,37 +1,63 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"context"
-	"time"
-	"os"
+	"flag"
+	"fmt"
 	"log"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/chromedp/chromedp"
+	"github.com/go-rod/rod/lib/launcher"
 )
 
 func main() {
-	siteURL := "https://example.com"
+	siteURL := flag.String("url", "", "The URL of the site to scrape")
+	flag.Parse()
+
+	if *siteURL == "" {
+		log.Fatal("Please provide a URL using the -url flag")
+		return
+	}
+	targetUrl := *siteURL
+
+	if !strings.HasPrefix(targetUrl, "http://") && !strings.HasPrefix(targetUrl, "https://") {
+		targetUrl = "http://" + targetUrl
+	}
+
+	browserPath, found := launcher.LookPath()
+	if !found {
+		log.Fatal("Chrome/Chromium browser not found")
+	}
+
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", true),
+		chromedp.ExecPath(browserPath),
+	)
+
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
 
 	// Create context
-	ctx, cancel := chromedp.NewContext(context.Background())
+	ctx, cancel := chromedp.NewContext(allocCtx)
 	defer cancel()
 
 	// Create timeout
 	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
 	defer cancel()
 
-	fmt.Printf("Visiting site: %s\n", siteURL)
+	fmt.Printf("Visiting site: %s\n", targetUrl)
 
 	var htmlContent string
 	var imageByte []byte
 
 	err := chromedp.Run(ctx,
-		chromedp.Navigate(siteURL),
+		chromedp.Navigate(targetUrl),
+		chromedp.Sleep(2*time.Second),
 		chromedp.OuterHTML("html", &htmlContent),
 		chromedp.FullScreenshot(&imageByte, 90),
-
 	)
 
 	if err != nil {
@@ -44,7 +70,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Print HTML content length
-	fmt.Printf("HTML content length: %d\n", len(htmlContent))
+	//save HTML content to file
+	err = os.WriteFile("page.html", []byte(htmlContent), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("HTML content saved as page.html")
 	fmt.Println("Screenshot saved as screenshot.png")
 }
