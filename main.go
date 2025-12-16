f71928165c252bf35ebf6feb3ef35ca11b214546
Emirhan Sevmez <emirhansevmezz@gmail.com>
2025-12-16 13:55:46 +0300

    feat: update main.go to support file input for URLs and enhance output directory structure

--- main.go
+++ main.go
@@ -16,25 +16,24 @@ import (
 
 func main() {
 	siteURL := flag.String("url", "", "The URL of the site to scrape")
+	file := flag.String("file", "", "For file scan (example: sites.txt)")
 	flag.Parse()
 
-	if *siteURL == "" {
-		log.Fatal("Please provide a URL using the -url flag")
+	if *siteURL == "" && *file == "" {
+		fmt.Println("Error: Please provide -url or -file")
 		return
 	}
-	targetUrl := *siteURL
-
-	if !strings.HasPrefix(targetUrl, "http://") && !strings.HasPrefix(targetUrl, "https://") {
-		targetUrl = "http://" + targetUrl
+	var targetUrls []string
+	if *siteURL != "" {
+		targetUrls = append(targetUrls, *siteURL)
 	}
+	if *file != "" {
+		fileContent, err := os.ReadFile(*file)
+		if err != nil {
+			log.Fatalf("Failed to read file: %v", err)
+		}
+		targetUrls = strings.Split(string(fileContent), "\n")
 
-	domainName := cleanup(targetUrl)
-
-	outputDir := filepath.Join("results", domainName)
-
-	err := os.MkdirAll(outputDir, os.ModePerm)
-	if err != nil {
-		log.Fatalf("Failed to create output directory: %v", err)
 	}
 
 	browserPath, found := launcher.LookPath()
@@ -58,37 +57,17 @@ func main() {
 	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
 	defer cancel()
 
-	fmt.Printf("Visiting site: %s\n", targetUrl)
-
-	var htmlContent string
-	var imageByte []byte
-
-	err = chromedp.Run(ctx,
-		chromedp.Navigate(targetUrl),
-		chromedp.Sleep(2*time.Second),
-		chromedp.OuterHTML("html", &htmlContent),
-		chromedp.FullScreenshot(&imageByte, 90),
-	)
-
-	if err != nil {
-		log.Fatal(err)
+	for _, targetUrl := range targetUrls {
+		targetUrl = strings.TrimSpace(targetUrl)
+		if targetUrl == "" {
+			continue
+		}
+		if !strings.HasPrefix(targetUrl, "http://") && !strings.HasPrefix(targetUrl, "https://") {
+			targetUrl = "https://" + targetUrl
+		}
+		fmt.Printf("Scanning : %s\n", targetUrl)
+		scanSite(ctx, targetUrl)
 	}
-	htmlPath := filepath.Join(outputDir, "source.html")
-	screenshotPath := filepath.Join(outputDir, "screenshot.png")
-
-	// Save screenshot to file
-	err = os.WriteFile(screenshotPath, imageByte, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//save HTML content to file
-	err = os.WriteFile(htmlPath, []byte(htmlContent), 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("HTML content saved as ", htmlPath)
-	fmt.Println("Screenshot saved as ", screenshotPath)
 }
 
 func cleanup(url string) string {
@@ -98,4 +77,44 @@ func cleanup(url string) string {
 	cleaned = strings.Split(cleaned, "/")[0]
 	cleaned = strings.Split(cleaned, "?")[0]
 	return cleaned
+}
+
+func scanSite(ctx context.Context, url string) {
+	cleanedURL := cleanup(url)
+	outputDir := filepath.Join("output", cleanedURL)
+	err := os.MkdirAll(outputDir, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var htmlContent string
+	var imageByte []byte
+	ctx, cancel := chromedp.NewContext(ctx)
+	defer cancel()
+	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
+	defer cancel()
+
+	err = chromedp.Run(ctx,
+		chromedp.Navigate(url),
+		chromedp.Sleep(2*time.Second),
+		chromedp.OuterHTML("html", &htmlContent),
+		chromedp.FullScreenshot(&imageByte, 90),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	htmlPath := filepath.Join(outputDir, "source.html")
+	screenshotPath := filepath.Join(outputDir, "screenshot.png")
+
+	err = os.WriteFile(screenshotPath, imageByte, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = os.WriteFile(htmlPath, []byte(htmlContent), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("HTML content saved as %s\n", htmlPath)
+	fmt.Printf("Screenshot saved as %s\n", screenshotPath)
 }
